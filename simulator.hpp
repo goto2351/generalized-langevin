24998ece67a02d76afcca2976646c846b37bfe58
goto2351 <gto9682@yahoo.co.jp>
2021-01-09 15:21:24 +0900

    Simulatorクラスを作成

---
+++ simulator.hpp
@@ -0,0 +1,20 @@
+#ifndef SIMULATOR_HPP
+#define SIMULATOR_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <array>
+#include <random>
+#include "./toml11/toml.hpp"
+#include "./particle.hpp"
+#include "./new_particle.hpp"
+
+namespace generalized_langevin {
+    class Simulator {
+
+    };//Simulator
+}//generalized_langevin
+
+#endif
